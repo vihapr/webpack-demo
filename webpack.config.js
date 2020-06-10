abafb9c16d9632c7c736e3fda96c4bf1e461d550
tank <exppro1@gmail.com>
2020-06-10 21:09:13 +0300

    A html-webpack-plugin was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    source: path.join(__dirname, 'src'),
+    target: path.join(__dirname, 'dist'),
+}
 
 module.exports = {
-  entry: './src/index.js',
+  entry: PATHS.source + '/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: PATHS.target,
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({title: 'Webpack app'})
+  ]
+
 };
